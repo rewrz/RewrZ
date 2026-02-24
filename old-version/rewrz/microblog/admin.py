97aa58b318303a53e0e3136fb2dc53735643554c
Rewrite;Zeroth <8691380+rewrz@users.noreply.github.com>
2026-02-24 16:26:44 +0800

    删除旧版本代码
    旧版本的已经归档打包过，删除旧版本代码

--- old-version/rewrz/microblog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Tweet, Topic
-# Register your models here.
-
-class TweetAdmin(admin.ModelAdmin):
-    list_display = ['author', 'content', 'created_time', 'modified_time']
-
-admin.site.register(Tweet, TweetAdmin)
-admin.site.register(Topic)
--- old-version/rewrz/microblog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MicroblogConfig(AppConfig):
-    name = 'microblog'
-    verbose_name = '微博客'
